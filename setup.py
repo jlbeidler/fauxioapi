734dcdf74cb31a23a3eb3762cfce2872c9ab64a2
James Beidler <james.beidler@gmail.com>
2023-07-06 09:17:45 -0400

    Dependency version fixes

--- setup.py
+++ setup.py
@@ -5,11 +5,14 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 setup(
     name="fauxioapi",
-    version="0.1.4",
-    packages = find_packages(),
+    version="0.1.6",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     python_requires='>=3.5',
-    setup_requires=['numpy>=1.16','netCDF4>=1.2.9'],
-    install_requires=['numpy>=1.16','netCDF4>=1.2.9'],
+    setup_requires=['numpy>=1.16,<=1.24.3','netCDF4>=1.2.9,<=1.5.8'],
+    install_requires=['numpy>=1.16,<=1.24.3','netCDF4>=1.2.9,<=1.5.8'],
+    scripts=['scripts/gen_grid_shape.py','scripts/regrid.py'],
+    package_data={'fauxioapi': ['data/griddesc.txt']},
     url="https://github.com/jlbeidler/fauxioapi",
     author_email='james.beidler@gmail.com',
     description="A simple module reproducing limited I/O API functions",
